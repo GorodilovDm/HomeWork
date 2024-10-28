c9b2505d9c1ce75a76497a48b431f5b6b0b9c389
GorodilovDm <gorodilovdm@gmail.com>
2024-10-28 20:52:34 +0700

    28.10.24 кортеж

---
+++ modul_1_5.py
@@ -0,0 +1,5 @@
+immutable_var = (1, 2, 3, 'String', [1, 2, 'A'])
+print('Immutable_var:', immutable_var)
+mutable_list = [1, 2, 3, 'String']
+mutable_list[3] = 'Dmitry'
+print('Mutable_list', mutable_list)
