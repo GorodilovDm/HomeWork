c5b0b1413ebff11c0a3eceea79e934638bdd41c1
GorodilovDm <gorodilovdm@gmail.com>
2024-10-28 21:15:18 +0700

    Add files via upload

---
+++ modul_1_3_1.py
@@ -0,0 +1,8 @@
+number_of_completed_homework = 12
+number_of_spent_hours = 1.5
+cours_name = 'Python'
+time_for_1_task = number_of_spent_hours / number_of_completed_homework
+
+print('Курс: ' + cours_name + ',' + ' всего задач:', number_of_completed_homework, ',' +
+      ' затрачено часов:', number_of_spent_hours, ',' +
+      ' среднее время выполнения:', time_for_1_task, 'часа')
