78c358c22a4279392232bdf951e730996f89b5c6
GorodilovDm <gorodilovdm@gmail.com>
2024-10-31 13:15:47 +0700

    Update modul_1_3_1.py

--- modul_1_3_1.py
+++ modul_1_3_1.py
@@ -6,3 +6,7 @@ time_for_1_task = number_of_spent_hours / number_of_completed_homework
 print('Курс: ' + cours_name + ',' + ' всего задач:', number_of_completed_homework, ',' +
       ' затрачено часов:', number_of_spent_hours, ',' +
       ' среднее время выполнения:', time_for_1_task, 'часа')
+
+print(f'Курс: {cours_name}, всего задач: {number_of_completed_homework},'
+      f' затрачено часов: {number_of_spent_hours}, '
+      f'среднее время выполнения: {time_for_1_task} часа')
