53c7d2c92b820fed81cc1081fb7ceaf8cd11f249
GorodilovDm <gorodilovdm@gmail.com>
2025-01-12 17:25:17 +0700

    11.01.25

---
+++ modul_10/modul_10_3.py
@@ -0,0 +1,39 @@
+import threading
+from time import sleep
+from random import randint
+
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for i in range(100):
+            dep = randint(50, 501)
+            self.balance += dep
+            print(f'Пополнение: {dep}. Баланс: {self.balance}')
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            sleep(0.001)
+
+    def take(self):
+        for i in range(100):
+            take_ = randint(50, 501)
+            print(f'Запрос на {take_}')
+            if take_ <= self.balance:
+                self.balance -= take_
+                print(f'Снятие: {take_}. Баланс: {self.balance}')
+            else:
+                print('Запрос отклонён, недостаточно средств')
+                self.lock.acquire()
+
+
+bk = Bank()
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+print(f'Итоговый баланс: {bk.balance}')
