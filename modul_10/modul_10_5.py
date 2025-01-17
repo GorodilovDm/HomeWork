e428a2e5f819f3a14de1455b991faec94d7a5440
GorodilovDm <gorodilovdm@gmail.com>
2025-01-17 21:19:59 +0700

    17.01.25

---
+++ modul_10/modul_10_5.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from multiprocessing import Pool
+
+
+def read_info(name):
+    all_data = []
+    with open(name) as file:
+        while True:
+            string = file.readline()
+            if string != '':
+                all_data.append(string)
+            else:
+                break
+
+
+if __name__ == '__main__':
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+    time_start = datetime.now()
+    for name in filenames:
+        read_info(name)
+    time_stop = datetime.now()
+    print(time_stop - time_start)
+
+    time_start = datetime.now()
+    with Pool() as pool:
+        pool.map(read_info, filenames)
+    time_stop = datetime.now()
+    print(time_stop - time_start)
